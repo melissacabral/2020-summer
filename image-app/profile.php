9e39e762a8f7c74e2c547c0af0c6f40cf3b79054
Melissa Cabral <melissacabral@gmail.com>
2020-08-05 10:16:40 -0700

    added posts to user profile

--- image-app/profile.php
+++ image-app/profile.php
@@ -1,13 +1,18 @@
 <?php require('config.php'); ?>
-<?php require('includes/header.php'); 
+<?php require('includes/header.php');?>
 
+<main class="content">	
+
+<?php 
 //which user's profile are we trying to show?
 //url will look like profle.php?user_id=X
 if( isset( $_GET['user_id'] ) ){
 	$user_id = $_GET['user_id'];
-}else{
+}elseif( $logged_in_user ){
 	//if no query string variable, use the logged in person
 	$user_id = $logged_in_user['user_id'];
+}else{
+	$user_id = 0;	
 }
 
 //get their info from the DB
@@ -25,21 +30,46 @@ if( $result->num_rows >= 1 ){
 		//new technique (will create $profile_pic, $username, $bio)
 		extract($row);		
 	}
-}
 ?>
-<main class="content">	
+
 	<section class="user user-profile">
 		<img src="<?php echo $profile_pic; ?>" class="profile-pic">
 		<h2><?php echo $username; ?></h2>
 		<p><?php echo $bio; ?></p>
 	</section>
 
-
+	<?php //get 20 most recent of the user's posts 
+	$sql = "SELECT * FROM posts 
+		WHERE user_id = $user_id 
+		AND is_published = 1 
+		LIMIT 20";
+	$result = $db->query($sql);
+	if( !$result ){
+		echo $db->error;
+	}
+	if( $result->num_rows >= 1 ){
+	?>
 	<div class="grid">
-		TODO: get this user's posts from the DB
+		<?php while( $row = $result->fetch_assoc() ){ ?>
+		<div class="item">
+			<a href="single.php?post_id=<?php echo $row['post_id']; ?>">
+				<?php display_post_image($row['post_id']); ?>
+			</a>
+			<h3><?php echo $row['title']; ?></h3>
+		</div>
+		<?php } //end while ?>
 	</div>
+	<?php 
+	}else{
+		echo 'This user hasn\'t posted anything yet.';
+	}//end if posts found ?>
+
+<?php 
+}else{
+	echo 'Invalid User';
+} //end if user found 
+?>
 
 </main>
-
 <?php require('includes/sidebar.php'); ?>		
 <?php require('includes/footer.php'); ?>		
